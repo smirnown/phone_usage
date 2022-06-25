0a0060b6d5e2059389dd22f9e420d3de30babb55
smirnown <45834392+smirnown@users.noreply.github.com>
2022-06-24 19:03:47 -0600

    Read phone usage statistics and calculate billing/charges per customer (#1)
    * Read phone usage statistics and calculate billing/charges per customer
    
    * update .gitignore
    
    * delete .idea directory
    
    * Fix some floating-point rounding issues
    
    * Add doc strings

---
+++ main.py
@@ -0,0 +1,184 @@
+import datetime
+from decimal import Decimal
+import math
+
+
+def run_billing(file):
+    """Reads csv file containing phone usage statistics, outputs billing/charges per customer in csv.
+
+    Args:
+        file (str): Path/name of input file containing usage statistics
+    """
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    call_infos = []
+    rows = lines[1:]
+    for row in rows:
+        values = row.split(",")
+        call_infos.append(CallInfo(
+            account_number=values[0],
+            origination_number=values[1],
+            termination_number=values[2],
+            call_start=values[3],
+            call_stop=values[4].strip(),  # strip newline character off end of row
+        ))
+
+    bills = calculate_bills(call_infos)
+
+    with open("output.csv", "w") as f:
+        headers = "account_number,minutes_international,number_international,minutes_domestic,number_domestic,minutes_local,number_local,charge\n"
+        f.write(headers)
+        for bill in bills:
+            f.write(str(bill) + "\n")
+
+
+def calculate_bills(call_infos):
+    """Calculate bills/charges for calls. Aggregate results by customer/account_number.
+
+    Args:
+        call_infos (list[CallInfo]): Phone usage statistics for individual calls
+    """
+    account_number_to_customer_bill_map = dict()
+
+    for info in call_infos:
+        bill = CustomerBill(info)
+        if account_number_to_customer_bill_map.get(info.account_number) is None:
+            account_number_to_customer_bill_map[info.account_number] = bill
+        else:
+            account_number_to_customer_bill_map[info.account_number].update(bill)
+
+    return list(account_number_to_customer_bill_map.values())
+
+
+class PhoneNumber:
+    """Simple data structure for interacting with phone numbers.
+
+    Args:
+        phone_number (str): Phone numbers take the form +(1-digit-country-code)(3-digit-area-code)(7-digits)
+    """
+
+    def __init__(self, phone_number):
+        phone_number = phone_number.strip("+")
+        self.country_code = phone_number[0]
+        self.area_code = phone_number[1:4]
+        self.number = phone_number[4:]
+
+
+class CustomerBill:
+    """Object representing a given customer/account's activity and charges.
+
+    Args:
+        call_info (CallInfo): Phone usage statistics for a single call
+    """
+
+    def __init__(self, call_info):
+        self.account_number = call_info.account_number
+        self.minutes_international = 0
+        self.num_international = 0
+        self.minutes_domestic = 0
+        self.num_domestic = 0
+        self.minutes_local = 0
+        self.num_local = 0
+        self.set_call_metrics(call_info)
+        self.charge = call_info.calculate_charge_for_call()
+
+    def set_call_metrics(self, call_info):
+        """Sets metrics for a given CustomerBill object upon instantiation
+
+        Args:
+            call_info (CallInfo): Phone usage statistics for a single call
+        """
+        duration = call_info.get_call_duration()
+        call_type = call_info.get_call_type()
+        if call_type == "international":
+            self.minutes_international += duration
+            self.num_international += 1
+        elif call_type == "domestic":
+            self.minutes_domestic += duration
+            self.num_domestic += 1
+        elif call_type == "local":
+            self.minutes_local += duration
+            self.num_local += 1
+        else:
+            raise NotImplementedError(f"Unhandled call_type encountered: {call_type}")
+
+    def update(self, new_call_info):
+        """Aggregate billing metrics from two CustomerBill objects into one
+
+        Args:
+            new_call_info (CustomerBill): Bill to merge with current bill
+        """
+        msg = f"Cannot combine bills for different accounts! " \
+              f"This account: {self.account_number}, Other account: {new_call_info.account_number}"
+        assert self.account_number == new_call_info.account_number, msg
+
+        self.minutes_international += new_call_info.minutes_international
+        self.minutes_domestic += new_call_info.minutes_domestic
+        self.minutes_local += new_call_info.minutes_local
+        self.num_international += new_call_info.num_international
+        self.num_domestic += new_call_info.num_domestic
+        self.num_local += new_call_info.num_local
+        self.charge += new_call_info.charge.quantize(Decimal('1.00'))
+
+    def __repr__(self):
+        return f"{self.account_number},{self.minutes_international},{self.num_international},{self.minutes_domestic}," \
+               f"{self.num_domestic},{self.minutes_local},{self.num_local},{self.charge.quantize(Decimal('1.00'))}"
+
+
+class CallInfo:
+    """Represents usage statistics about a given call, and exposes methods to bill that call.
+
+    Args:
+        account_number (str): ID of account making call
+        origination_number (str): Number making call
+        termination_number (str): Number being called
+        call_start (str): Time call started
+        call_stop (str): Time call ended
+    """
+
+    def __init__(self, account_number, origination_number, termination_number, call_start, call_stop):
+        self.account_number = account_number
+        self.origination_number = origination_number
+        self.termination_number = termination_number
+        self.call_start = call_start
+        self.call_stop = call_stop
+
+    def get_call_duration(self):
+        """Find how long the call lasted in minutes, rounded up."""
+        start = datetime.datetime.fromisoformat(self.call_start)
+        stop = datetime.datetime.fromisoformat(self.call_stop)
+        difference = stop - start
+        return math.ceil(difference.seconds / 60)
+
+    def get_call_type(self):
+        """Determine whether call was international, domestic, or local."""
+        origin_number = PhoneNumber(self.origination_number)
+        termination_number = PhoneNumber(self.termination_number)
+        if origin_number.country_code != termination_number.country_code:
+            return "international"
+        elif origin_number.area_code != termination_number.area_code:
+            return "domestic"
+        else:
+            return "local"
+
+    def calculate_charge_for_call(self):
+        """Determine how much to charge account for the call."""
+        duration = self.get_call_duration()
+        call_type = self.get_call_type()
+        if call_type == "international":
+            base = 1
+            rate = .2
+        elif call_type == "domestic":
+            base = 0
+            rate = .1
+        elif call_type == "local":
+            base = 0
+            rate = .02
+        else:
+            raise NotImplementedError(f"Unhandled call_type encountered: {call_type}")
+        return Decimal(base + rate * duration).quantize(Decimal('1.00'))
+
+
+if __name__ == "__main__":
+    run_billing("usage.csv")
---
+++ test_main.py
@@ -0,0 +1,202 @@
+import pytest
+import main
+from decimal import Decimal
+
+
+@pytest.fixture(scope="session")
+def international_call_info():
+    yield main.CallInfo(
+        account_number="1",
+        origination_number="+15555555555",
+        termination_number="+26666666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:11.696409"
+    )
+
+
+@pytest.fixture(scope="session")
+def domestic_call_info():
+    yield main.CallInfo(
+        account_number="1",
+        origination_number="+15555555555",
+        termination_number="+16666666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:11.696409"
+    )
+
+
+@pytest.fixture(scope="session")
+def local_call_info():
+    yield main.CallInfo(
+        account_number="1",
+        origination_number="+15555555555",
+        termination_number="+15556666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:11.696409"
+    )
+
+
+def test_get_call_duration_no_rounding(international_call_info):
+    assert international_call_info.get_call_duration() == 2
+
+
+def test_get_call_duration_round_up():
+    call_info = main.CallInfo(
+        account_number="1",
+        origination_number="+15555555555",
+        termination_number="+16666666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:12.696409"
+    )
+    assert call_info.get_call_duration() == 3
+
+
+def test_get_call_type_international(international_call_info):
+    assert international_call_info.get_call_type() == "international"
+
+
+def test_get_call_type_domestic(domestic_call_info):
+    assert domestic_call_info.get_call_type() == "domestic"
+
+
+def test_get_call_type_local(local_call_info):
+    assert local_call_info.get_call_type() == "local"
+
+
+def test_calculate_international_charge(international_call_info):
+    assert international_call_info.calculate_charge_for_call() == Decimal(1.4).quantize(Decimal('1.00'))
+
+
+def test_calculate_domestic_charge(domestic_call_info):
+    assert domestic_call_info.calculate_charge_for_call() == Decimal(.2).quantize(Decimal('1.00'))
+
+
+def test_calculate_local_charge(local_call_info):
+    assert local_call_info.calculate_charge_for_call() == Decimal(.04).quantize(Decimal('1.00'))
+
+
+def test_phone_number(local_call_info):
+    number = main.PhoneNumber(local_call_info.termination_number)
+    assert number.country_code == "1"
+    assert number.area_code == "555"
+    assert number.number == "6666666"
+
+
+def test_international_customer_bill(international_call_info):
+    bill = main.CustomerBill(international_call_info)
+    assert bill.minutes_international == 2
+    assert bill.num_international == 1
+    assert bill.minutes_domestic == 0
+    assert bill.num_domestic == 0
+    assert bill.minutes_local == 0
+    assert bill.num_local == 0
+    assert bill.charge == Decimal(1.4).quantize(Decimal('1.00'))
+
+
+def test_domestic_customer_bill(domestic_call_info):
+    bill = main.CustomerBill(domestic_call_info)
+    assert bill.minutes_international == 0
+    assert bill.num_international == 0
+    assert bill.minutes_domestic == 2
+    assert bill.num_domestic == 1
+    assert bill.minutes_local == 0
+    assert bill.num_local == 0
+    assert bill.charge == Decimal(.2).quantize(Decimal('1.00'))
+
+
+def test_local_customer_bill(local_call_info):
+    bill = main.CustomerBill(local_call_info)
+    assert bill.minutes_international == 0
+    assert bill.num_international == 0
+    assert bill.minutes_domestic == 0
+    assert bill.num_domestic == 0
+    assert bill.minutes_local == 2
+    assert bill.num_local == 1
+    assert bill.charge == Decimal(.04).quantize(Decimal('1.00'))
+
+
+def test_update_customer_bill(international_call_info, domestic_call_info):
+    bill = main.CustomerBill(international_call_info)
+    domestic_bill = main.CustomerBill(domestic_call_info)
+
+    bill.update(domestic_bill)
+
+    assert bill.minutes_international == 2
+    assert bill.num_international == 1
+    assert bill.minutes_domestic == 2
+    assert bill.num_domestic == 1
+    assert bill.minutes_local == 0
+    assert bill.num_local == 0
+    assert bill.charge.quantize(Decimal('1.00')) == Decimal(1.6).quantize(Decimal('1.00'))
+
+
+def test_update_customer_bill_with_different_account_number(international_call_info):
+    bill = main.CustomerBill(international_call_info)
+    call_info = main.CallInfo(
+        account_number="2",
+        origination_number="+15555555555",
+        termination_number="+16666666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:12.696409"
+    )
+    other_bill = main.CustomerBill(call_info)
+    with pytest.raises(AssertionError):
+        bill.update(other_bill)
+
+
+def test_calculate_single_bill(international_call_info):
+    bills = main.calculate_bills([international_call_info])
+    assert len(bills) == 1
+
+    bill = bills[0]
+    assert bill.minutes_international == 2
+    assert bill.num_international == 1
+    assert bill.minutes_domestic == 0
+    assert bill.num_domestic == 0
+    assert bill.minutes_local == 0
+    assert bill.num_local == 0
+    assert bill.charge == Decimal(1.4).quantize(Decimal('1.00'))
+
+
+def test_calculate_multiple_bills_for_one_account(international_call_info, domestic_call_info):
+    bills = main.calculate_bills([international_call_info, domestic_call_info])
+    assert len(bills) == 1
+
+    bill = bills[0]
+    assert bill.minutes_international == 2
+    assert bill.num_international == 1
+    assert bill.minutes_domestic == 2
+    assert bill.num_domestic == 1
+    assert bill.minutes_local == 0
+    assert bill.num_local == 0
+    assert bill.charge == Decimal(1.6).quantize(Decimal('1.00'))
+
+
+def test_calculate_multiple_bills_for_multiple_accounts(international_call_info, domestic_call_info):
+    other_account_call_info = main.CallInfo(
+        account_number="2",
+        origination_number="+15555555555",
+        termination_number="+16666666666",
+        call_start="2022-06-24 15:31:11.696409",
+        call_stop="2022-06-24 15:33:11.696409"
+    )
+    bills = main.calculate_bills([international_call_info, domestic_call_info, other_account_call_info])
+    assert len(bills) == 2
+
+    account_1_bill = bills[0]
+    assert account_1_bill.minutes_international == 2
+    assert account_1_bill.num_international == 1
+    assert account_1_bill.minutes_domestic == 2
+    assert account_1_bill.num_domestic == 1
+    assert account_1_bill.minutes_local == 0
+    assert account_1_bill.num_local == 0
+    assert account_1_bill.charge == Decimal(1.6).quantize(Decimal('1.00'))
+
+    account_2_bill = bills[1]
+    assert account_2_bill.minutes_international == 0
+    assert account_2_bill.num_international == 0
+    assert account_2_bill.minutes_domestic == 2
+    assert account_2_bill.num_domestic == 1
+    assert account_2_bill.minutes_local == 0
+    assert account_2_bill.num_local == 0
+    assert account_2_bill.charge == Decimal(.2).quantize(Decimal('1.00'))
